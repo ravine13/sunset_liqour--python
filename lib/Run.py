537efc3d89f147797a94eaed502696765dc54037
ravine13 <kuriaderek@gmail.com>
2023-12-15 08:08:42 +0300

    added query for all categories

---
+++ lib/Run.py
@@ -0,0 +1,31 @@
+import click
+from sqlalchemy import and_
+from Main import session, Customer, Brand, Rating, Comment
+from sqlalchemy import func  # Make sure to import func
+
+@click.command()
+def find_customer():
+    customer_name = click.prompt(click.style("Enter the customer name", fg='blue'), type=str)
+
+    customer = session.query(Customer).filter(func.lower(Customer.name) == func.lower(customer_name)).first()
+
+    if customer:
+        click.echo(click.style(f"Customer Name: {customer.name}, Email: {customer.email}", fg='green'))
+
+        ratings = session.query(Rating).filter(Rating.customer_id == customer.id).all()
+        comments = session.query(Comment).filter(Comment.customer_id == customer.id).all()
+
+        for rating in ratings:
+            brand = session.query(Brand).filter(Brand.id == rating.brand_id).first()
+            if brand:
+                click.echo(f"Chosen Drink: {brand.name}")
+                click.echo(f"Rating: {rating.score}")
+
+        for comment in comments:
+            click.echo(f"Customer Comment: {comment.text}")
+    else:
+        click.echo("No matching customer found.")
+
+if __name__ == '__main__':
+    find_customer()
+
